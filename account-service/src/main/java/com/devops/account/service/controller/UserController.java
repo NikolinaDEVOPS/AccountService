7d8a35d30d99e88a19e6e3bccddfcf97dcf8c33b
nikolinaapetrovic <nikolinaapetrovic97@gmail.com>
2021-07-01 19:16:24 +0200

    [AS-04] Account crud added (#8)

--- account-service/src/main/java/com/devops/account/service/controller/UserController.java
+++ account-service/src/main/java/com/devops/account/service/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.devops.account.service.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,18 +25,56 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/")
-	public User savePost(@RequestBody User post) {
+	public User savePost(@RequestBody User user) {
 		log.info("savePost method of PostController");
-		return userService.save(post);
+		return userService.save(user);
 	}
 	
-	@GetMapping("/{id}")
-	public User findOne(@PathVariable Integer id) {
-	    return userService.findById(id);
+	@GetMapping("/{username}")
+	public User findOne(@PathVariable String username) {
+	    return userService.findByUsername(username);
 	}
 	
-	@GetMapping("/{id}/post")
+	@GetMapping("/public")
+	public List<User> findAllPublic() {
+	    return userService.findByIsPublic();
+	}
+	
+	@GetMapping("/{username}/isPublic")
+	public Boolean chekIfPublic(@PathVariable String username) {
+	    return userService.findByUsername(username).getIsPublic();
+	}
+	
+	@PutMapping("/{username}")
+	public User updateProfile(@RequestBody User newUser, @PathVariable String username) {
+		User user = userService.findByUsername(username);
+		
+		if(user == null) return null;
+		
+		newUser.setUserId(user.getUserId());
+		return userService.save(newUser);
+	}
+	
+	@PutMapping("/{username}/setPublic")
+	public User setIsPublic(@PathVariable String username) {
+		User user = userService.findByUsername(username);
+		
+		if(user == null) return null;
+		user.setIsPublic(true);
+		return userService.save(user);
+	}
+	
+	@PutMapping("/{username}/setPrivate")
+	public User setPrivate(@PathVariable String username) {
+		User user = userService.findByUsername(username);
+		
+		if(user == null) return null;
+		user.setIsPublic(false);
+		return userService.save(user);
+	}
+	
+	/*@GetMapping("/{id}/post")
 	public ResponseVO getUserWithost(@PathVariable Integer id) {
 		return userService.getUserWithPost(id);
-	}
+	}*/
 }
